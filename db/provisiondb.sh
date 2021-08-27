60529f23f9020aeac48800d5ea667b5ffc7c3706
ZeeshanJ99 <88186084+ZeeshanJ99@users.noreply.github.com>
2021-08-27 18:54:03 +0100

    27/08 updates

---
+++ db/provisiondb.sh
@@ -0,0 +1,25 @@
+!#/bin/bash
+
+# provisioning script
+
+sudo apt-get update -y
+sudo apt-get upgrade -y
+
+# Add MongoDB Repo
+wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
+sudo apt-get update
+
+# install and verify
+sudo apt-get install -y mongodb-org
+
+# start MongoDB
+sudo systemctl start mongod
+
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv D68FA50FEA312927
+echo "deb https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
+
+sudo apt-get update -y
+sudo apt-get upgrade -y
+
+sudo apt-get install -y mongodb-org=3.2.20 mongodb-org-server=3.2.20 mongodb-org-shell=3.2.20 mongodb-org-mongos=3.2.20 mongodb-org-tools=3.2.20
--- provision.sh
+++ provision.sh
@@ -5,3 +5,21 @@ sudo apt-get update -y
 sudo apt-get upgrade -y
 
 sudo apt-get install nginx -y
+
+# install and setup npm and nodejs
+sudo apt-get install npm -y
+sudo apt-get install python-software-properties -y
+curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+sudo apt-get install nodejs -y
+
+# change directory to app
+cd /home/ubuntu/app/app
+sudo npm install pm2 -get
+npm install
+
+export DB_HOST=192.168.10.150:27017/posts
+echo "export DB_HOST=192.168.10.150:27017/posts" >> ~/.bashrc
+source ~/.bashrc
+
+sudo nginx -tee
+sudo systemctl restart nginx
